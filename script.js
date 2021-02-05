58bb56006ef9f239bbb98b9afcdc155742ea4aec
innab <innabraun08@gmail.com>
2021-02-05 18:05:12 +0100

    homework 7

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 
 function createList(list,parent=document.body){
-let elem=document.createElement("ul")
+//let elem=document.createElement("ul")
 
 
-    list.map((itemElem)=>{
+    /*list.map((itemElem)=>{
         let li=document.createElement("li");
         li.innerText=itemElem
         elem.append(li)
     })
     parent.append(elem)
+*/
+
+    let resultArray=list.map((itemElem)=>{
+       return `<li>${itemElem}</li>`
+
+    })
+
+    const newString=resultArray.join(" ");
+    parent.insertAdjacentHTML("afterbegin",`<ul>${newString}</ul>`);
+
+
+
 
 }
+
+
+
+
 
 createList(["hello","today","is","beautiful","day"],document.body);
